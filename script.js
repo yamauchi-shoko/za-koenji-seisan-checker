8b0b58f77c005be611c68f4775a2518ba3a8cb89
yamauchi-shoko <yamauchi@syuzgen.com>
2026-07-02 16:32:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.getElementById("testButton").addEventListener("click", () => {
-  alert("動きました！");
+const button = document.getElementById("checkButton");
+
+button.addEventListener("click", () => {
+
+    document.getElementById("result").innerHTML = `
+
+        <h2>チェック結果</h2>
+
+        <p>🚧 まだ開発中です。</p>
+
+        <p>Version 1ではここにチェック結果が表示されます。</p>
+
+    `;
+
 });
